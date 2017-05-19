e48329f109f6b5ad335fb83f499a161540bf71fc
rubens22 <rubensrodrigues22@gmail.com>
2017-05-19 14:29:29 -0300

    Fiz a criação da activity lista_estacionamento, Começei a classe java Lista_Estacionamentos e EstacionamentoDao

---
+++ app/src/main/java/br/com/acheivaga/acheivaga/Lista_Estacionamento.java
@@ -0,0 +1,27 @@
+package br.com.acheivaga.acheivaga;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by ruben on 19/05/2017.
+ */
+
+public class Lista_Estacionamento extends Activity implements AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_estacionamentos);
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+}
---
+++ app/src/main/java/br/com/acheivaga/acheivaga/model/EstacionamentoDao.java
@@ -0,0 +1,8 @@
+package br.com.acheivaga.acheivaga.model;
+
+/**
+ * Created by ruben on 19/05/2017.
+ */
+
+public class EstacionamentoDao {
+}
