6ab935c46cbe3ac9e7c08a3bbbf4dd3854b508f3
Wanderson <wanderson_os@otlook.com>
2017-05-28 15:55:49 -0300

    Criação dos Getts e Setts dos atributos da classe "CadastrarCliente"

--- app/src/main/java/br/com/acheivaga/acheivaga/CadastarCliente.java
+++ app/src/main/java/br/com/acheivaga/acheivaga/CadastarCliente.java
@@ -16,4 +16,92 @@ public class CadastarCliente {
     private String Cidade;
     private String Usuario;
     private String senha;
+
+    public String getNomeProprietario() {
+        return NomeProprietario;
+    }
+
+    public void setNomeProprietario(String nomeProprietario) {
+        NomeProprietario = nomeProprietario;
+    }
+
+    public String getNomeEstacionamento() {
+        return NomeEstacionamento;
+    }
+
+    public void setNomeEstacionamento(String nomeEstacionamento) {
+        NomeEstacionamento = nomeEstacionamento;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        Endereco = endereco;
+    }
+
+    public String getNúmero() {
+        return Número;
+    }
+
+    public void setNúmero(String número) {
+        Número = número;
+    }
+
+    public String getQtdVagas() {
+        return QtdVagas;
+    }
+
+    public void setQtdVagas(String qtdVagas) {
+        QtdVagas = qtdVagas;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String bairro) {
+        Bairro = bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String cidade) {
+        Cidade = cidade;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        Usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
