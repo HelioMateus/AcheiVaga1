2ceeb68fdb39fa87f7b6dac060cfac8f77d780bd
Wanderson <wanderson_os@otlook.com>
2017-05-28 15:01:34 -0300

    Criação da classe "CadastrarCliente"; Declaração das variáveis da classe CadastrarCliente; Criação do botão "Efetuar Cadastro".

---
+++ app/src/main/java/br/com/acheivaga/acheivaga/CadastarCliente.java
@@ -0,0 +1,19 @@
+package br.com.acheivaga.acheivaga;
+
+/**
+ * Created by Wanderson on 28/05/2017.
+ */
+
+public class CadastarCliente {
+    private String NomeProprietario;
+    private String NomeEstacionamento;
+    private String CNPJ;
+    private String Telefone;
+    private String Rua;
+    private String Número;
+    private String QtdVagas;
+    private String Bairro;
+    private String Cidade;
+    private String Usuario;
+    private String senha;
+}
