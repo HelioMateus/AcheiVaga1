7fe530e03051963cea038084f8fa95aeb18334fd
Wanderson <wanderson_os@otlook.com>
2017-05-29 23:58:34 -0300

    formatação layout cadastrar

--- app/src/main/java/br/com/acheivaga/acheivaga/Usuario.java
+++ app/src/main/java/br/com/acheivaga/acheivaga/Usuario.java
@@ -16,7 +16,6 @@ public class Usuario {
     private String Cidade;
     private String Email;
     private String senha;
-    private String teste;
 
     public String getNomeProprietario() {
         return NomeProprietario;
